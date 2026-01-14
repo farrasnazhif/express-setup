e46f0d8f6fce4561e1bb6ac537180aa80408efb8
farrasnazhif <farrasnazhif@gmail.com>
2026-01-14 09:11:24 +0700

    feat: cors setup

--- server.ts
+++ server.ts
@@ -1,8 +1,12 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
+
+const port = process.env.PORT || 5173;
 
 const app = express();
 
-const port = 3000;
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is Live!");
